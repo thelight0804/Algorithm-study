4b7ea6a1fb7e342a76feaf4328f59fb5e5ae17a5
thelight0804 <thelight0804@gmail.com>
2022-11-10 23:15:29 +0900

    ea feat: Add BAEKJOON Num2742 python answer
    - Num2742 : 기찍 N

---
+++ Python/class01/Num2742.py
@@ -0,0 +1,21 @@
+"""
+Date:
+  22.11.10
+Title:
+  BAEKJOON 2742번
+Project:
+  기찍 N
+Level:
+  Bronze 4
+Name:
+  thelight0804
+"""
+
+# standard_input = '5'
+
+#input num
+num = int(input())
+
+#print num
+for i in range(num, 0, -1):
+  print(i)
