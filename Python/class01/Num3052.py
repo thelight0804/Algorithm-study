335ed4f9e1911f0d8058135a183aebcacd8da16d
thelight0804 <thelight0804@gmail.com>
2022-11-13 21:26:40 +0900

    feat: add BAEKJOON Num3052 python answer
    - Num3052 : 나머지

---
+++ Python/class01/Num3052.py
@@ -0,0 +1,51 @@
+"""
+Date:
+  22.11.13
+Title:
+  BAEKJOON 3052번
+Project:
+  나머지
+Level:
+  Bronze 2
+Name:
+  thelight0804
+"""
+standard_input = """39
+40
+41
+42
+43
+44
+82
+83
+84
+85
+"""
+
+#input num
+a = [int(input()) for i in range (10)]
+remain = []
+
+for i in range(10):
+  temp = a[i] % 42
+  #find same div remain
+  try:
+    remain.index(temp)
+  #append div remain when not value
+  except ValueError:
+    remain.append(temp)
+
+print(len(remain))
+
+# #input num
+# a = [int(input()) for i in range (10)]
+# arr = []
+
+# #input div remain to arr
+# for i in range(10):
+#   arr.append(a[i] % 42)
+
+# #set array
+# remain = set(arr)
+
+# print(len(remain))
