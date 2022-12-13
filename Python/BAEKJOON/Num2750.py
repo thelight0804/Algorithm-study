4511223a228722cea6b7c12da031a9eeff96fa9e
thelight0804 <thelight0804@gmail.com>
2022-12-13 21:14:20 +0900

    Add BAEKJOON Num15596 python answer
    - Num15596 : 정수 N개의 합

--- Python/BAEKJOON/Num2750.py
+++ Python/BAEKJOON/Num2750.py
@@ -2,27 +2,14 @@
 Date:
   22.12.13
 Title:
-  BAEKJOON 2750번
+  BAEKJOON 15596번
 Project:
-  수 정렬하기
+  정수 N개의 합
 Level:
   Bronze 2
 Name:
   thelight0804
 """
-# standard_input="""5
-# 5
-# 2
-# 3
-# 4
-# 1
-# """
-# input test case and number
-test = int(input())
-num = [int(input()) for i in range (test)]
 
-num.sort()
-
-# print num List
-for i in range(test):
-  print(num[i])
+def solve(a):
+  return sum(a)
