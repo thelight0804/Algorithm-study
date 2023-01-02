1b73176c7500143e7b490adc2edbc879d71f5cca
thelight0804 <thelight0804@gmail.com>
2023-01-02 14:50:28 +0900

    Simplify code in Num1712

--- Python/BAEKJOON/Num1712.py
+++ Python/BAEKJOON/Num1712.py
@@ -13,14 +13,10 @@ Name:
 # standard_input = '3 2 1'
 
 #input a, b, c
-num = list(map(int, input().split()))
-a = num[0]
-b = num[1]
-c = num[2]
+a, b, c = map(int, input().split())
 
 if(b >= c): #can not BREAK-EVEN POINT
   print(-1)
 else: #calculate BREAK-EVEN POINT
   income = c - b
-  i = int(a / income)
-  print(i+1)
+  print(int(a / income)+1)
