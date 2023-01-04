4322f7ea9f7aa7e16c55d6ffe88866b331eecc3f
thelight0804 <thelight0804@gmail.com>
2023-01-04 21:13:51 +0900

    Add BAEKJOON Num1193 python answer
    - Num1193 : 분수찾기
    - Num1110과 Num4344의 내용과 파일 이름을 서로 바꾸었습니다

---
+++ Python/BAEKJOON/Num1193.py
@@ -0,0 +1,36 @@
+"""
+Date:
+  22.01.04
+Title:
+  BAEKJOON 1193번
+Project:
+  분수찾기
+Level:
+  Bronze 1
+Name:
+  thelight0804
+"""
+# standard_input = """4"""
+
+#input num
+num = int(input())
+
+#check line, max number of line
+#1 / 2,3 / 4,5,6 / 7,8,9,10
+line = 0
+maxLine = 0
+while (num > maxLine):
+  line += 1
+  maxLine += line
+
+lineGap = maxLine - num
+
+#calculate fraction
+#if line is even number
+if line % 2 == 0:
+  up = line - lineGap
+  down = lineGap + 1
+else: # if line is odd number
+  up = lineGap + 1
+  down = line - lineGap
+print(f'{up}/{down}')
