f29e364a632760e63127b0e396ec26d21a37e8c5
thelight0804 <thelight0804@gmail.com>
2023-01-04 15:14:22 +0900

    Add BAEKJOON Num4344 python answer
    - Num4344 : 평균은 넘겠지

--- Python/BAEKJOON/Num1110.py
+++ Python/BAEKJOON/Num1110.py
@@ -1,35 +1,37 @@
 """
 Date:
-  22.01.02
+  22.01.04
 Title:
-  BAEKJOON 1110번
+  BAEKJOON 4344번
 Project:
-  더하기 사이클
+  평균은 넘겠지
 Level:
   Bronze 1
 Name:
   thelight0804
 """
-# standard_input = '71'
+# standard_input = """5
+# 5 50 50 70 80 100
+# 7 100 95 90 80 70 60 50
+# 3 70 90 80
+# 3 70 90 81
+# 9 100 99 98 97 96 95 94 93 91"""
 
 #input number
-num = int(input())
-d = num
+test = int(input())
+for i in range(test):
+  num = list(map(int, input().split()))
+  student = num[0]
 
-cnt = 0 #count
+  #calculate average
+  avg = sum(num[1:]) / student
 
-while(True):
-  cnt += 1 #increase count 
+  #check students score above average
+  count = 0
+  for j in range(student):
+    if(num[j+1] > avg):
+      count +=1
 
-  # a + b = c
-  a = d//10
-  b = d % 10
-  c = (a + b) % 10
-
-  # new number
-  d = (b * 10) + c
-
-  #print result count
-  if(num == d):
-    print(cnt)
-    break
+  #print result
+  rate = count / student * 100
+  print(f'{rate:.3f}%')
