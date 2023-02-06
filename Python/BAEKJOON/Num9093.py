dfad2f68b2d67f6534c356ad74ff23be894276c3
thelight0804 <thelight0804@gmail.com>
2023-02-06 17:49:48 +0900

    Add BAEKJOON problem 9093 python answer
    - Num9093 : 단어 뒤집기

---
+++ Python/BAEKJOON/Num9093.py
@@ -0,0 +1,39 @@
+"""
+Date:
+  23.02.06
+Title:
+  BAEKJOON 9093번
+Project:
+  단어 뒤집기
+Level:
+  Bronze 1
+Name:
+  thelight0804
+"""
+# standard_input = """2
+# I am happy today
+# We want to win the first prize"""
+
+#input word
+test = int(input())
+
+for _ in range(test):
+  #input string
+  word = input().split()
+  for i in word:
+    #output reverse slice
+    print(i[::-1], end=" ")
+  print("")
+
+#Use stack
+"""
+for _ in range(test):
+  word = input().split() #input String
+  for i in range(len(word)):
+    for j in range(len(word[i])):
+      stack.append(word[i][j]) #push stack
+    for j in range(len(stack)):
+      print(stack.pop(), end="") #pop stack
+    print(" ", end="")
+"""
+
