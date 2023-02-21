1343aceccda4d0c239467921a7fb7b614ab58bb2
thelight0804 <thelight0804@gmail.com>
2023-02-22 08:40:57 +0900

    Add Baekjoon problem 10430 python answer
    Num10430 : 나머지

---
+++ Python/BAEKJOON/Num10430.py
@@ -0,0 +1,24 @@
+"""
+Date:
+  23.02.22
+Title:
+  BAEKJOON 10430번
+Project:
+  나머지
+Level:
+  Bronze 5
+Name:
+  thelight0804
+"""
+standard_input = """5 8 4"""
+#1 (A+B)%C
+#1 ((A%C) + (B%C))%C
+#0 (A×B)%C
+#0 ((A%C) × (B%C))%C
+
+a, b, c = map(int, input().split())
+
+print((a+b)%c)
+print(((a%c) + (b%c))%c)
+print((a*b)%c)
+print(((a%c) * (b%c))%c)
