af924a929cdfb1fc38b778f5c2d0ef91ae456f26
thelight0804 <thelight0804@gmail.com>
2023-03-03 10:52:08 +0900

    >> Add BAEKJOON problem 2839 python answer >> >> Num2839 : 설탕 배달

---
+++ Python/BAEKJOON/Num2839.py
@@ -0,0 +1,55 @@
+"""
+Date:
+  23.03.03
+Title:
+  BAEKJOON 2839번
+Project:
+  설탕 배달
+Level:
+  Silver 4
+Name:
+  thelight0804
+"""
+standard_input = """804"""
+# 4
+
+
+
+### DP
+n = int(input())
+
+l = [0] * (n+1) # memoization
+
+# DP Bottom-Up
+for i in range(3, n+1):
+  # fixed number
+  if i == 3 or i == 5:
+    l[i] = 1
+  elif i == 4 or i == 7:
+    l[i] = -1
+
+  # add 5kg suger
+  elif i % 5 == 0:
+    l[i] = l[i-5] + 1
+  else: # add 3kg suger
+    l[i] = l[i-3] + 1
+
+print(l[i])
+
+
+### Calculation
+"""
+n = int(input())
+bag = 0
+while n >= 0:
+   #add 5kg
+  if n % 5 == 0:
+    bag += (n // 5)
+    print(bag)
+    break
+  else: #add 3kg
+    n -= 3
+    bag += 1
+else:
+  print(-1)
+"""
