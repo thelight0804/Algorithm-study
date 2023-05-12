8e3f0501713ed2603b7c57c6b012e57cf9ced79c
thelight0804 <thelight0804@gmail.com>
2023-05-12 23:55:26 +0900

    Add LeetCode problem 242 python answer
    Valid Anagram

---
+++ Python/LeetCode/Num242.py
@@ -0,0 +1,45 @@
+"""
+Date:
+  23.05.12
+Number:
+  LeetCode 242
+Title:
+  Valid Anagram
+Level:
+  Easy
+Name:
+  thelight0804
+"""
+
+class Solution(object):
+  def isAnagram(self, s, t):
+    """
+    :type s: str
+    :type t: str
+    :rtype: bool
+    """
+    table = {}
+
+    # add character count of c
+    for c in s:
+      if table.get(c) is None:
+        table[c] = 1
+      else:
+        table[c] += 1
+    
+    # sub character count of t
+    for c in t:
+      #not exist character
+      if table.get(c) is None:
+        return False
+      else:
+        table[c] -= 1
+        #not match character count
+        if table[c] < 0: return False
+    
+    #not match character count
+    for i in table.values():
+      if i != 0: return False
+    
+    #match anagram
+    return True
