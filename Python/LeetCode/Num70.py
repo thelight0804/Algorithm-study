cdc9bcbdbd902a2790c70e220e4db16dab97b6d9
thelight0804 <thelight0804@gmail.com>
2023-05-17 12:12:20 +0900

    Refactor code for improved readability and update comments
    leetcode 70번 문제에서 코드를 간결하게 수정하였고
    자연스러운 영어 주석으로 변경했습니다

--- Python/LeetCode/Num70.py
+++ Python/LeetCode/Num70.py
@@ -18,8 +18,9 @@ class Solution(object):
     :rtype: int
     """
     #memoization
-    f = [0 for i in range(n+1)]
+    f = [0] * (n+1)
     for i in range(1, n+1):
+      #fixed values
       if i == 1: f[1] = 1
       elif i == 2: f[2] = 2
       else: #dynamic algorithm
