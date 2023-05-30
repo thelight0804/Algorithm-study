ebbda55b146aee3d43645993919d982d93cb15e8
thelight0804 <thelight0804@gmail.com>
2023-05-30 10:08:01 +0900

    Add Python solution for LeetCode problem 412
    Fizz Buzz

---
+++ Python/LeetCode/Num412.py
@@ -0,0 +1,34 @@
+"""
+Date:
+  23.05.30
+Number:
+  LeetCode 412
+Title:
+  Fizz Buzz
+Level:
+  Easy
+Name:
+  thelight0804
+"""
+
+ class Solution(object):
+  def fizzBuzz(self, n):
+    """
+    :type n: int
+    :rtype: List[str]
+    """
+    result = [str(i+1) for i in range(n)]
+
+    #divisible by 3
+    for i in range(2, n, 3):
+      result[i] = "Fizz"
+    
+    #divisible by 5
+    for i in range(4, n, 5):
+      result[i] = "Buzz"
+    
+    #divisible by 3 and 5
+    for i in range(14, n, 15):
+      result[i] = "FizzBuzz"
+    
+    return result
