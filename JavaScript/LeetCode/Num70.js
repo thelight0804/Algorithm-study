f2022aa6cadc682bd79ea11ba998de17f7540dad
thelight0804 <thelight0804@gmail.com>
2023-09-15 14:18:07 +0900

    Add JavaScript solution for LeetCode problem 70
    Climbing Stairs

---
+++ JavaScript/LeetCode/Num70.js
@@ -0,0 +1,24 @@
+/**
+ * Date: 23.09.15
+ * Number: LeetCode 70
+ * Title: Climbing Stairs
+ * Level: Easy
+ * Author: thelight0804
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    var a = 1; // first step
+    var b = 1; // second step
+
+    for (var i = 1; i < n; i++){
+        var tmp = a;
+        a += b; // add second step to a
+        b = tmp;
+    }
+    
+    return n > 0 ? a : 1;
+};
